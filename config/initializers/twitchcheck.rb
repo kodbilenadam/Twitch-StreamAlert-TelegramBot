b0ee9db12e25fa37f0e21c2f6cf29f64e2b464ce
Unknown <osmankaya8@hotmail.com>
2017-08-23 15:17:23 +0300

    twitchcheck works now..
    without threads eheh

--- config/initializers/twitchcheck.rb
+++ config/initializers/twitchcheck.rb
@@ -1,41 +1,27 @@
-# require "rest-client"
-# require "json"
-# puts "Sunucular aktifleştiriliyor"
-# thr = Thread.new do
-#   loop do
-#     online = {}
-#     online.default = 0
-#     @server = Server.all
-#     puts @server
-#     @server.each do |a|
-#       cevap = RestClient.get("https://api.twitch.tv/kraken/streams/#{a.name}", headers={'Client-ID': 'hndmr9nq1m74r78whfmj3v04m0jvbc'})
-#       puts "Parsing"
-#       icerik = JSON.parse(cevap)
-#       puts online
-#       if icerik['stream'] != nil
-#         online[a.name] += 1
-#       else
-#         online[a.name] = 0
-#       end
-#     end
-#     sleep(10)
-#
-#   end
-# end
-# puts "Second Thread starting"
-# Thread.new do
-#   loop do
-#     puts "Basliyo loop"
-#     online.each do |server, status|
-#       puts status
-#       if status == 1
-#         Server.find_by(name: server).users.all.each do |user|
-#           bot.send_message chat_id: user.channel_id, text: "#{server} yayın açtı."
-#           puts "Yayın açtı alert"
-#
-#         end
-#       end
-#     end
-#     sleep(2)
-#   end
-# end
+require "rest-client"
+require "json"
+require 'telegram/bot'
+puts "Sunucular aktifleştiriliyor"
+thr = Thread.new do
+    fakequeue = 0
+    @online = {}
+    @online.default = 0
+  loop do
+    @servers = Server.all
+    @servers.each do |s|
+    cevap = RestClient.get("https://api.twitch.tv/kraken/streams/#{s.name}", headers={'Client-ID': 'hndmr9nq1m74r78whfmj3v04m0jvbc'})
+    icerik = JSON.parse(cevap)
+    if icerik['stream'] != nil
+      @online[s.name] += 1
+      if @online[s.name] == 1
+        s.users.all.each do |user|
+          Telegram.bot.send_message chat_id: user.channel_id, text: "#{s.name} yayın açtı."
+        end
+      end
+    else
+      @online[s.name] = 0
+    end
+  end
+    sleep(10)
+  end
+end
